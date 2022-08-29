3820559e27b839da7216945b61ac26e5e29d759a
royal0817 <martinezl27737@gmail.com>
2022-08-29 11:51:09 -0400

    syled and sperated buttons before reworking resume

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     <Services />
     <Portfolio />
     {/* <Testemonials /> */}
-    <Contact />
+    {/* <Contact /> */}
     <Footer />
     </>
   );
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import CV from '../images/Resume_PDF_format.pdf'
 
 const Resume = () => {
   return (
     <div className='resume'>
-    <a href={CV} download className='btn'>Download CV</a>
+    <a href={CV} download className='btn'>Open CV</a>
     <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
     )
